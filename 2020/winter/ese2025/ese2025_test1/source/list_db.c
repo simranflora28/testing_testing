a615e647fcae779b49de1438071d51f8d8f936fb
SIMRAN FLORA <57839965+simranflora28@users.noreply.github.com>
2020-04-20 15:17:06 -0400

    Update list_db.c

--- 2020/winter/ese2025/ese2025_test1/source/list_db.c
+++ 2020/winter/ese2025/ese2025_test1/source/list_db.c
@@ -97,8 +97,11 @@ stuLL_t* findLinkedList(stuLL_t *pHEAD, char *targetFamilyName)
 		// list is not empty, begin search
 		while ((pW != NULL) && (strcmp(pW->data.fName, targetFamilyName)))
 		{
-			// PROBLEM #2: complete the code for this function
-			//	       NOTE: PLEASE COMMIT CHANGES ONCE YOUR DONE THIS FUNCTION
+			// PROBLEM #2: SOLUTION
+			
+			// JUMP TO THE NEXT NODE
+			pW = pW->pNext;
+			
 		}
 		printf("\n");
 	}
