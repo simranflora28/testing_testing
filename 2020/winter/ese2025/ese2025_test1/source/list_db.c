ea1d209f86ffec6027e69b18ac03f40cda426628
SIMRAN FLORA <57839965+simranflora28@users.noreply.github.com>
2020-04-20 15:06:51 -0400

    Update list_db.c

--- 2020/winter/ese2025/ese2025_test1/source/list_db.c
+++ 2020/winter/ese2025/ese2025_test1/source/list_db.c
@@ -19,8 +19,10 @@
  */
 stuLL_t* createNodeLinkedList(void)
 {
-	// PROBLEM #1: complete the code for this function
-	//	       NOTE: PLEASE COMMIT CHANGES ONCE YOUR DONE THIS FUNCTION
+	// PROBLEM 1: SOLUTION
+	
+	stull_t* pNew= (stull_t*malloc(sizeof(stull_t));
+	return pNew;
 }
 
 /*
